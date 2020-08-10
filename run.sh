d1e5fdb8e2d21a7254f9d199b36fd02d5697b87e
Tuan Vu <contact@tuanva.co.uk>
2020-08-10 14:11:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 mv .env.sample .env
 
 read -p 'IG Username: ' user
-sed -i -e"s/^IG_USERNAME=.*/IG_USERNAME=$user/" .env
+sed -i '' -e"s/^IG_USERNAME=.*/IG_USERNAME=$user/" .env
 
 read -sp 'IG Password: ' pass
-sed -i -e"s/^IG_PASSWORD=.*/IG_PASSWORD=$pass/" .env
+sed -i '' -e"s/^IG_PASSWORD=.*/IG_PASSWORD=$pass/" .env
 
 echo 'Starting the app...'
 
